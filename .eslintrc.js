667844e497b3d7de9e2e4bd9a64fbcd862d0df7f
goldenriver1592 <havohoang3@gmail.com>
2025-04-18 07:26:51 +0700

    add all

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'cypress'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:cypress/recommended',
+    ],
+    env: {
+      node: true,
+      browser: true,
+      'cypress/globals': true,
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': 'off',
+    },
+    ignorePatterns: ['node_modules/', 'dist/', 'cypress/videos/', 'cypress/screenshots/'],
+  }
+  
---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    excludeSpecPattern: ['cypress/e2e/1-getting-started/*.js', 'cypress/e2e/2-advanced-examples/*.js',]
+  },  
+});
