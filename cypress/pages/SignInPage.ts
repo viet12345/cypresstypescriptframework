bbf55bdad60eb642dadfdfe41251d8ecb432d965
viet12345 <vietdh42@wru.vn>
2025-04-23 10:05:42 +0700

    Login Page-Kiểm tra các element hiển thị trên page

--- cypress/pages/SignInPage.ts
+++ cypress/pages/SignInPage.ts
@@ -1,11 +1,27 @@
+const LoginPageSelectors = {
+  usernameInput: "#username",
+  passwordInput: "#password",
+  loginButton: "[data-test='signin-submit']",
+  loginTitle: ".MuiTypography-h5"
+};
+
 export class SignInPage {
   usernameInputBox() {
-    return cy.get("#username");
+    return cy.get(LoginPageSelectors.usernameInput);
   }
 
   passwordInputBox() {
-    return cy.get("#password");
+    return cy.get(LoginPageSelectors.passwordInput);
   }
+
+  loginButton() {
+    return cy.get(LoginPageSelectors.loginButton);
+  }
+
+  verifyTitleLoginPage(titleLoginPage:string) {
+    return cy.get(LoginPageSelectors.loginTitle).should('have.text', titleLoginPage);
+  }
+  
 }
 
 export const signInPage = new SignInPage();
--- cypress/tests/integration/demo.cy.ts
+++ cypress/tests/integration/demo.cy.ts
@@ -2,11 +2,18 @@ import { PAGE_URLS } from '../../fixtures/urls'
 import { signInPage } from '../../pages/index.page'
 
 describe('my first demo', () => {
-    before('redirect to the login page of demo guru', () => {
+    beforeEach('redirect to the login page of demo guru', () => {
         cy.visit(PAGE_URLS.SIGNIN_PAGE);
     })
 
     describe('login with valid credentials', () => {
+        
+        it('Kiểm tra các element hiển thị trên page', () => {
+            signInPage.passwordInputBox().should('be.visible');
+            signInPage.usernameInputBox().should('be.visible');
+            signInPage.loginButton().should('be.visible');
+            signInPage.verifyTitleLoginPage('Sign in').should('be.visible');
+        })
 
         it('verify user is able to login with valid credentials', () => {
             signInPage.passwordInputBox().should('be.visible');
