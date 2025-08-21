51e491bdae52949418944e262b9997dcef95363d
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-08-21 16:11:58 +0700

    Update dragDropAction to verify success

--- cypress/tests/SaleCRM/dragDropAction.cy.ts
+++ cypress/tests/SaleCRM/dragDropAction.cy.ts
@@ -7,22 +7,37 @@ describe('Check drag drop action', () => {
     })
 
     it.only(`Update deal Approach -> Engaged`, () => {
+        const dealSourceSelector = '[data-deal-id="10376"] > .deal__item--detail';
+        const dealTargetSelector = '#dealItemGridArea_2';
+
         //Mở page có chứa search function
         cy.visit('deals'); // Cập nhật đường dẫn nếu cần
 
-        // 1. Drag task sang Done (có xử lý animation delay)
-        cy.dragAndDrop('[data-deal-id="10513"] > .deal__item--detail', '#dealItemGridArea_2');
-
-        // 2. Verify modal xuất hiện
-        cy.get('#popupConfirmMovingDealToNewStage > .modal-dialog > .modal-content').should('be.visible');
-        cy.get('#buttonAcceptMovingDeal').click();
-
-        // 3. Verify deal đã được chuyển sang stage mới
-        cy.get('iframe[id="content_ifr"]').first().then($iframe => {
-            const body = $iframe.contents().find('body');
-            cy.wrap(body).type('First Meeting (Approach -> Engaged)');
-        });
-        cy.get('input[class="filepond--browser"]').first().attachFile(`DataTestingFiles/download.jpeg`); // Đính kèm file ảnh nếu required
-        cy.get('button[class="milestone__button--cancel"]').contains('Cancel').click({ force: true });
+        //Lấy deal name để sử dụng verify move stage thành công sau khi drag and drop
+        cy.get(dealSourceSelector).then($dealItem => {
+            cy.wrap($dealItem).find('.item__name').invoke('text').then($dealName => {
+                const expectedDealName = $dealName.trim();
+                // 1. Drag task sang Done (có xử lý animation delay)
+                cy.dragAndDrop(dealSourceSelector, dealTargetSelector)
+        
+                // 2. Verify modal xuất hiện
+                cy.get('#popupConfirmMovingDealToNewStage > .modal-dialog > .modal-content').should('be.visible');
+                cy.get('#buttonAcceptMovingDeal').click();
+        
+                // 3. Nhập thông tin note và đính kèm file.
+                cy.get('iframe[id="content_ifr"]').first().then($iframe => {
+                    const body = $iframe.contents().find('body');
+                    cy.wrap(body).type('First Meeting (Approach -> Engaged)');
+                });
+                cy.get('input[class="filepond--browser"]').first().attachFile(`DataTestingFiles/download.jpeg`); // Đính kèm file ảnh nếu required
+                cy.get('button[class="milestone__button--save"]').contains('Save').click({ force: true });
+                cy.reload();
+        
+                // 4. Verify deal đã được cập nhật stage
+                cy.get(dealTargetSelector).then($target => {
+                    cy.wrap($target).find('.item__name').first().should('contain.text', expectedDealName);
+                });
+            });
+        })
     });
 })
