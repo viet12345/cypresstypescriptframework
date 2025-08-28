1a2012fe97138265de2a0de0da301c0082214b71
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-08-28 13:16:54 +0700

    Update uploadeFile_v2: Remove duplicated code.

--- cypress/tests/SaleCRM/uploadeFile_v2.cy.ts
+++ cypress/tests/SaleCRM/uploadeFile_v2.cy.ts
@@ -1,3 +1,25 @@
+function uploadFileAndVerify(fileName: any) {
+    // Tìm button/input để upload file
+    cy.get('#show-notes').should('be.visible').click();
+    cy.get('.button--add-note').should('be.visible').click();
+    cy.get('.form__note--add-new > :nth-child(5) > .form__input').should('be.empty').type('Upload file test by cypress ' + fileName);
+    cy.get('iframe[class="tox-edit-area__iframe"]').first().then($iframe => {
+        const body = $iframe.contents().find('body');
+        cy.wrap(body).type('Upload file test by cypress');
+    });
+    cy.get('input[class="filepond--browser"]').first().attachFile({filePath: `DataTestingFiles/${fileName}`,encoding: 'base64'}, {force:true});
+    
+    // Submit form nếu cần
+    cy.get('.form__note--add-new > .form__box--button > .button--save').click();
+
+    // Kiểm tra file đã được upload thành công
+    cy.get('.body__file-attachment--list > a', {timeout:10000}).should('have.attr', 'href');
+    cy.get('.box__header', {timeout: 10000}).first().then($firstNote => {
+        cy.wrap($firstNote).find('button[class="dropdown-toggle"]').click();
+        cy.get('button[class="dropdown-item button--delete-note"]').click();
+    })
+}
+
 describe('Kiểm tra upload tất cả file từ folder DataTestingFiles.', () => {
     if (!Cypress.env('fileList') || Cypress.env('fileList').length === 0) {
         throw new Error('Không tìm thấy file nào trong thư mục DataTestingFiles. Vui lòng thêm file vào thư mục này để chạy test.');
@@ -6,62 +28,26 @@ describe('Kiểm tra upload tất cả file từ folder DataTestingFiles.', () =
     console.log('Danh sách file cần kiểm tra:', Cypress.env('fileList'));
 
     beforeEach('Authentication steps', () => {
+        cy.clearSession();
         cy.saveLoginSession(); // Lưu session đăng nhập trước khi thực hiện các thao tác khác
     });
 
     describe('Upload file từ Contact detail.', () => {
     Object.entries(Cypress.env('fileList')).forEach(([index,fileName]) => {
-
-        it(`Kiểm tra upload file ${fileName} trong tạo Note từ Contact detail.`, () => {
-            cy.visit('contacts/49786'); // Cập nhật đường dẫn nếu cần
-
-            // Tìm button/input để upload file
-            cy.get('#show-notes').should('be.visible').click();
-            cy.get('.button--add-note').should('be.visible').click();
-            cy.get('.form__note--add-new > :nth-child(5) > .form__input').should('be.empty').type('Upload file test by cypress ' + fileName);
-            cy.get('iframe[class="tox-edit-area__iframe"]').first().then($iframe => {
-                const body = $iframe.contents().find('body');
-                cy.wrap(body).type('Upload file test by cypress');
+            it(`Kiểm tra upload file ${fileName} trong tạo Note từ Contact detail.`, () => {
+                cy.visit('contacts/49786'); // Cập nhật đường dẫn nếu cần
+                uploadFileAndVerify(fileName);
             });
-            cy.get('input[type="file"]').attachFile(`DataTestingFiles/${fileName}`);
-            // Submit form nếu cần
-            cy.get('.form__note--add-new > .form__box--button > .button--save').click();
-
-            // Kiểm tra file đã được upload thành công
-            cy.get('.body__file-attachment--list').contains(`${fileName}`).should('exist');
-            cy.get('.box__header').first().then($firstNote => {
-                cy.wrap($firstNote).find('button[class="dropdown-toggle"]').click();
-                cy.get('button[class="dropdown-item button--delete-note"]').click();
-            })
         });
-    });
     })
 
 
     describe('Upload file từ Deal detail.', () => {
     Object.entries(Cypress.env('fileList')).forEach(([index,fileName]) => {
         it(`Kiểm tra upload file ${fileName} trong tạo Note từ Deal detail.`, () => {
-        cy.visit('deals/10394'); // Cập nhật đường dẫn nếu cần
-
-        // Tìm button/input để upload file
-        cy.get('#show-notes').should('be.visible').click();
-        cy.get('.button--add-note').should('be.visible').click();
-        cy.get('.form__note--add-new > :nth-child(5) > .form__input').should('be.empty').type('Upload file test by cypress ' + fileName);
-        cy.get('iframe[class="tox-edit-area__iframe"]').first().then($iframe => {
-            const body = $iframe.contents().find('body');
-            cy.wrap(body).type('Upload file test by cypress');
+            cy.visit('deals/10394'); // Cập nhật đường dẫn nếu cần
+                uploadFileAndVerify(fileName);
+            });
         });
-        cy.get('input[type="file"]').attachFile(`DataTestingFiles/${fileName}`);
-        // Submit form nếu cần
-        cy.get('.form__note--add-new > .form__box--button > .button--save').click();
-
-        // Kiểm tra file đã được upload thành công
-        cy.get('.body__file-attachment--list').contains(`${fileName}`).should('exist');
-        cy.get('.box__header').first().then($firstNote => {
-            cy.wrap($firstNote).find('button[class="dropdown-toggle"]').click();
-            cy.get('button[class="dropdown-item button--delete-note"]').click();
-            })
-        });
-    });
     });
 });
