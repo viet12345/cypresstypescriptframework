1bf7a395d667b53318ecf920952eb369de21a199
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-09-04 17:01:01 +0700

    Test validateIntergrateData

---
+++ cypress/tests/SaleCRM/validateIntergrateData.cy.ts
@@ -0,0 +1,40 @@
+describe('Kiểm tra intergrated data', () => {
+    //Authentication steps: Lưu cookies/session để sử dụng lại trong các test khác.
+    beforeEach('Authentication steps', () => {
+        cy.clearSession();
+        // Cách 1: Thiết lập giá trị cookie trực tiếp
+        cy.saveLoginSession();
+        // Cách 2: Với các hệ thống có chức năng login cơ bản, nên sử dụng hàm LoginbyApi từ command.
+    })
+
+    describe('Kiểm tra data list', () => {
+        it(`Kiểm tra data Contact`, () => {
+            const CONTACT_LIST_API_ENDPOINT:string = 'https://sales-crm-dev.adamo.tech/contacts/get-all-contact-by-current-role';
+            const MOCK_DATA_CONTACT_LIST:any = require('../../fixtures/SaleCRM/contactList.json');
+
+            //Mở page list contact
+            cy.visit('contacts'); // Cập nhật đường dẫn nếu cần
+
+            //Intercept API để chờ dữ liệu load xong (có thể mock dữ liệu nếu cần)
+            cy.intercept('GET', CONTACT_LIST_API_ENDPOINT, {
+                statusCode: 200,
+                body: MOCK_DATA_CONTACT_LIST,
+            }).as('getContacts');
+
+            //Lấy dữ liệu từ API
+            cy.wait('@getContacts').then((interception) => {
+                const CONTACT_LIST:any[] = interception.response?.body.data.data.data;
+                CONTACT_LIST.forEach((contact,index) => {
+                    const FULL_NAME = contact.first_name + ' ' + contact.last_name;
+                    const LIFE_CYCLE_STAGE = contact.stage.name;
+
+                    //Kiểm tra dữ liệu với UI đang hiển thị
+                    cy.get('#dataContactBind > tr').eq(index).within(() => {
+                        cy.get('.table__contact--name').should('contain', FULL_NAME);
+                        cy.get('.table__contact--stage').should('contain', LIFE_CYCLE_STAGE);
+                    })
+                });
+            });
+        })
+    })  
+})
