aca76c0dcd0aa3f856777b1576285b09c5d96cfd
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-09-17 16:58:50 +0700

    Tối ưu lại test intergrateData

--- cypress/tests/GQT/validateIntergrateData.cy.ts
+++ cypress/tests/GQT/validateIntergrateData.cy.ts
@@ -1,6 +1,19 @@
 describe('Kiểm tra intergrated data', () => {
+    const MOCK_DATA_GATEWAYS:any = require('../../fixtures/GQT/gateways.json');
+    const GATEWAYS = MOCK_DATA_GATEWAYS.map((gateway: any) => ({
+        code: gateway.code,
+        status: gateway.status
+    }));
+
+    const MOCK_DATA_DESTINATIONS:any = require('../../fixtures/GQT/destinations.json');
+    const DESTINATIONS = MOCK_DATA_DESTINATIONS.map((destination: any) => ({
+        airportCode:destination.airportCode,
+        code: destination.code,
+        status: destination.status
+    }));
+
     //Authentication steps: Lưu cookies/session để sử dụng lại trong các test khác.
-    before('Authentication steps', () => {
+    beforeEach('Authentication steps', () => {
         cy.clearSession();
         // Cách 1: Thiết lập giá trị cookie trực tiếp
         // Cách 2: Với các hệ thống có chức năng login cơ bản, nên sử dụng hàm LoginbyApi từ command.
@@ -14,25 +27,47 @@ describe('Kiểm tra intergrated data', () => {
             cy.get('#pv_id_11_53 > .role-options').contains('Lorelei Reid').click();
         });
         cy.get('#submit-user').click();
+        cy.visit('https://qalogin-gqt-spa.vacv-nonprod.click/qa-hxjhdknbdklnbklnbkjkld.html#/internal/packages'); // Cập nhật đường dẫn nếu cần
+    
+        cy.wait(30000);
+    
+        cy.log('success');
     });
+    
+    describe.only('Kiểm tra data Gateways có tồn tại và trạng thái checkbox hiển thị chính xác.', () => {
+        GATEWAYS.forEach(($gateway:any) => {
+            it(`Kiểm tra data Gateways ${$gateway.code}.`, () => {
+                cy.get('[pc53=""]').should('be.visible');
+                cy.get('[pc53=""] > .p-datatable-column-header-content > .p-datatable-filter > .p-button').click({ force: true });
+                cy.get('input[placeholder="Airport Code"]').type($gateway.code);
+                cy.get('button[aria-label="Apply"]').click({ force: true });
+                cy.wait(2000); // Chờ dữ liệu load xong
+                
+                //Kiểm tra dữ liệu Gateway tồn tại trên GUI
+                //Kiểm tra trạng thái check-box Package
+                cy.get('div[class="city-table"]').should('be.visible').within(() => {
+                    cy.get('tr').eq(1).then(($row) => {
+                        cy.wrap($row).find('td').eq(0).should('have.text', $gateway.code);
+                        cy.wrap($row).find('div[id="Package"]').eq(0).then(($checkbox) => {
+                            if ($gateway.status === 'OPEN') {
+                                expect($checkbox.attr('data-p-checked')).to.equal('true');
+                            }
+                            else
+                                expect($checkbox.attr('data-p-checked')).to.equal('false');
+                        });
+                    });
+                });
+
+                // //Clear data
+                // cy.get('[pc53=""] > .p-datatable-column-header-content > .p-datatable-filter > .p-button').click({ force: true });
+                // cy.get('input[placeholder="Airport Code"]').clear();
+                // cy.get('button[aria-label="Apply"]').click({ force: true });
+                // cy.wait(2000); // Chờ dữ liệu load xong
+            });
+        })
+    })
 
     it(`Kiểm tra data Gateways có tồn tại và trạng thái checkbox hiển thị chính xác.`, () => {
-        const API_ENDPOINT:string = 'https://vacations-qa-api.aircanada.com/gqt-mdt/api/city/list-management';
-        const MOCK_DATA_GATEWAYS:any = require('../../fixtures/GQT/gateways.json');
-        const GATEWAYS = MOCK_DATA_GATEWAYS.map((gateway: any) => ({
-                code: gateway.code,
-                status: gateway.status
-        }));
-        console.log('GATEWAYS_IDs:', GATEWAYS);
-
-        //Intercept API để chờ dữ liệu load xong (có thể mock dữ liệu nếu cần)
-        cy.intercept('GET', API_ENDPOINT).as('getPackage');
-
-        cy.visit('https://qalogin-gqt-spa.vacv-nonprod.click/qa-hxjhdknbdklnbklnbkjkld.html#/internal/packages'); // Cập nhật đường dẫn nếu cần
-        
-        cy.wait(40000);
-
-        cy.log('success');
 
         //Lấy dữ liệu từ JSON
         cy.get('[pc53=""]').should('be.visible').then(() => {
@@ -56,7 +91,7 @@ describe('Kiểm tra intergrated data', () => {
                         });
                     });
                 });
-
+    
                 //Clear data
                 cy.get('[pc53=""] > .p-datatable-column-header-content > .p-datatable-filter > .p-button').click({ force: true });
                 cy.get('input[placeholder="Airport Code"]').clear();
@@ -67,25 +102,7 @@ describe('Kiểm tra intergrated data', () => {
     })
 
 
-    it.only(`Kiểm tra data Destinations có tồn tại và trạng thái checkbox hiển thị chính xác.`, () => {
-        const API_ENDPOINT:string = 'https://vacations-qa-api.aircanada.com/gqt-mdt/api/city/list-management';
-        const MOCK_DATA_DESTINATIONS:any = require('../../fixtures/GQT/destinations.json');
-        const DESTINATIONS = MOCK_DATA_DESTINATIONS.map((destination: any) => ({
-                airportCode:destination.airportCode,
-                code: destination.code,
-                status: destination.status
-        }));
-        console.log('DESTINATIONS', DESTINATIONS);
-
-        //Intercept API để chờ dữ liệu load xong (có thể mock dữ liệu nếu cần)
-        cy.intercept('GET', API_ENDPOINT).as('getPackage');
-
-        cy.visit('https://qalogin-gqt-spa.vacv-nonprod.click/qa-hxjhdknbdklnbklnbkjkld.html#/internal/packages'); // Cập nhật đường dẫn nếu cần
-        
-        cy.wait(40000);
-
-        cy.log('success');
-
+    it(`Kiểm tra data Destinations có tồn tại và trạng thái checkbox hiển thị chính xác.`, () => {
         //Lấy dữ liệu từ JSON
         cy.get('[pc89=""]').then(() => {
             DESTINATIONS.forEach(($data:any) => {
