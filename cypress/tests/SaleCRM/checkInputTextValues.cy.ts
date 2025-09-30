201760431f6b03f93594c7798f1af2f786a63fe4
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-09-30 14:26:16 +0700

    Update SaleCRM: Tests for input text values.

--- cypress/tests/SaleCRM/checkInputTextValues.cy.ts
+++ cypress/tests/SaleCRM/checkInputTextValues.cy.ts
@@ -4,6 +4,19 @@ interface InputCase {
   error_mes?: string;
 }
 
+function typeInputAndVerify(btnOpenForm:string ,inputSelector: string, btnSaveForm:string , errorSelector: string, inputCase:InputCase) {
+    cy.get(btnOpenForm).should('be.visible').click().then(() => {
+    if (inputCase.value != "") {
+        // Nhập dữ liệu vào trường First name
+        cy.get(inputSelector).clear().type(inputCase.value); 
+    }
+    cy.get(btnSaveForm).click();
+    if (!inputCase.valid) {
+        cy.get(errorSelector).should('be.visible').and('contain.text', inputCase.error_mes);
+    }
+    });
+}
+
 describe('Check các input field với text values', () => {
     //Authentication steps: Lưu cookies/session để sử dụng lại trong các test khác.
     beforeEach('Authentication steps', () => {
@@ -14,26 +27,151 @@ describe('Check các input field với text values', () => {
         });
     })
 
-    describe('Check Add a new contact', () => {
+    describe('Check trong form Add a new contact', () => {
         const TEXT_DATA_INPUT = require('../../fixtures/SaleCRM/text_data_values.json');
-        const TEXT_INPUT = TEXT_DATA_INPUT.text_input;
+        const FIRST_NAME_INPUT = TEXT_DATA_INPUT.first_name_input;
+        const LAST_NAME_INPUT = TEXT_DATA_INPUT.last_name_input;
         const EMAIL_INPUT = TEXT_DATA_INPUT.email_input;
+        const PHONE_NUMBER_INPUT = TEXT_DATA_INPUT.phone_without_code_input;
+        const LINK_INPUT = TEXT_DATA_INPUT.dynamic_link_input;
         
-        Object.entries(TEXT_INPUT).forEach(([$case, $valueObj]) => {
+        Object.entries(FIRST_NAME_INPUT).forEach(([$case, $valueObj]) => {
             it(`Kiểm tra nhập First name với input ${$case}`, () => {
                 const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#first_name';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#first_name-error';
+
                 cy.visit('/contacts');
-                cy.get('#btn-add-contact').should('be.visible').click().then(() => {
-                    if (inputCase.value != "") {
-                        // Nhập dữ liệu vào trường First name
-                        cy.get('#first_name').clear().type(inputCase.value); 
-                    }
-                    cy.get('#formContact > .form-contact__button > #save-contact').click();
-                    if (!inputCase.valid) {
-                        cy.get('#first_name-error').should('be.visible').and('contain.text', inputCase.error_mes);
-                    }
-                });
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
             });
         })
+
+        Object.entries(LAST_NAME_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Last name với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#last_name';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#last_name-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(EMAIL_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Email với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#email';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#email-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(PHONE_NUMBER_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Phone number với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '[name="phone"]';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#phone-no-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(LINK_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập LINK_INPUT với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#linked_in';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#linked_in-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+    })
+
+    describe('Check trong form Edit a contact', () => {
+        const TEXT_DATA_INPUT = require('../../fixtures/SaleCRM/text_data_values.json');
+        const FIRST_NAME_INPUT = TEXT_DATA_INPUT.first_name_input;
+        const LAST_NAME_INPUT = TEXT_DATA_INPUT.last_name_input;
+        const EMAIL_INPUT = TEXT_DATA_INPUT.email_input;
+        const PHONE_NUMBER_INPUT = TEXT_DATA_INPUT.phone_without_code_input;
+        const LINK_INPUT = TEXT_DATA_INPUT.dynamic_link_input;
+        
+        Object.entries(FIRST_NAME_INPUT).forEach(([$case, $valueObj]) => {
+            it.only(`Kiểm tra nhập First name với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '.button-edit';
+                const inputSelector = '[name="first_name"]';
+                const btnSaveForm = '#btnSaveEditContact';
+                const errorSelector = '#first_name-error';
+
+                cy.visit('/contacts/312');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        })
+
+        Object.entries(LAST_NAME_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Last name với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#last_name';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#last_name-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(EMAIL_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Email với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#email';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#email-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(PHONE_NUMBER_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập Phone number với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '[name="phone"]';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#phone-no-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
+
+        Object.entries(LINK_INPUT).forEach(([$case, $valueObj]) => {
+            it(`Kiểm tra nhập LINK_INPUT với input ${$case}`, () => {
+                const inputCase = $valueObj as InputCase; // Ép kiểu tại đây
+                const btnOpenForm = '#btn-add-contact';
+                const inputSelector = '#linked_in';
+                const btnSaveForm = '#formContact > .form-contact__button > #save-contact';
+                const errorSelector = '#linked_in-error';
+
+                cy.visit('/contacts');
+                typeInputAndVerify(btnOpenForm, inputSelector, btnSaveForm, errorSelector, inputCase);
+            });
+        });
     })
 })
