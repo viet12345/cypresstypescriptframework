a3b933bf4ff2bc72b4ba5d90b1a1f369a0e28af9
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2025-11-05 16:36:30 +0700

    SaleCRM: Optimize sortTest

--- cypress/tests/SaleCRM/sortTest.cy..ts
+++ cypress/tests/SaleCRM/sortTest.cy..ts
@@ -1,4 +1,52 @@
 import dayjs from 'dayjs'
+
+function verifySorting(dataType:'alphabet' | 'number' | 'date', orderBy:'asc'|'desc', sortColumn:string, tableCell:string) {
+    if (dataType==='alphabet'){
+        cy.get(sortColumn).should('have.attr', 'data-sort-type', `${orderBy}`).then(() => {
+            cy.get(`#dataContactBind ${tableCell}`).then($cells => {
+                const values = [...$cells].map(c => c.innerText.trim() || null);
+                const sorted = [...values].sort((a:any, b:any) => a.localeCompare(b, 'vi', { sensitivity: 'base' }));
+                expect(values).to.deep.eq(sorted);
+            })
+        });
+    }
+
+    if (dataType==='date'){
+        cy.get(sortColumn).should('have.attr', 'data-sort-type', `${orderBy}`).then(() => {
+            cy.get(`#dataContactBind ${tableCell}`).then($cells => {
+                const values = [...$cells].map(c => c.innerText.trim() || null);
+                if (orderBy === 'desc'){
+                    const sort = [...values].sort((a, b) => dayjs(b, 'DD/MM/YYYY').valueOf() - dayjs(a, 'DD/MM/YYYY').valueOf());
+                    expect(values).to.deep.equal(sort);
+                } else {
+                    const sort = [...values].sort((a, b) => dayjs(a, 'DD/MM/YYYY').valueOf() - dayjs(b, 'DD/MM/YYYY').valueOf());
+                    expect(values).to.deep.equal(sort);
+                }
+            })
+        });
+    }
+
+    if (dataType==='number'){
+        cy.get(sortColumn).should('have.attr', 'data-sort-type', `${orderBy}`).then(() => {
+            cy.get(`${tableCell}`).then($cells => {
+                const values = [...$cells].map(c => c.innerText.trim() || null);
+                const numericValues = values.map(v => v ? Number(v) : null);
+                if (orderBy === 'desc'){
+                    const sorted = [...numericValues].sort((a, b) => (b ?? -Infinity) - (a ?? -Infinity))
+                    expect(numericValues).to.deep.eq(sorted);
+                } else {
+                    const sorted = [...numericValues].sort((a, b) => (a ?? -Infinity) - (b ?? -Infinity))
+                    expect(numericValues).to.deep.eq(sorted);
+                }
+            })
+        });
+    }
+}
+
+//Khai báo các biến config/setup data.
+const Role = Cypress.env('Role');
+
+
 describe('Kiểm tra chức năng sorting', () => {
     //Authentication steps: Lưu cookies/session để sử dụng lại trong các test khác.
     beforeEach('Authentication steps', () => {
@@ -6,9 +54,25 @@ describe('Kiểm tra chức năng sorting', () => {
         cy.saveLoginSession();
         // Cách 2: Với các hệ thống có chức năng login cơ bản, nên sử dụng hàm LoginbyApi từ command.
     })
+    if (Role === 'Admin'){
+        describe('Sorting Kpis-point page', () => {
+            it('test số giảm dần', () => {
+                cy.visit(Cypress.env('SaleCRM_URL')+'kpis');
+                cy.get('th').contains('Monthly KPI goal').click();
+                verifySorting('number','desc','span[id="monthly_kpi_goal"]','[class^="goal-kpi-"]');
+            });
+
+            it('test số tăng dần', () => {
+                cy.visit(Cypress.env('SaleCRM_URL')+'kpis');
+                cy.get('th').contains('Monthly KPI goal').click();
+                cy.get('th').contains('Monthly KPI goal').click();
+                verifySorting('number','asc','span[id="monthly_kpi_goal"]','[class^="goal-kpi-"]');
+            });
+        })
+    }
 
     //Chỉ hoạt động với acc Staff role.
-    describe.only('Sorting Contact page', () => {
+    describe('Sorting Contact page', () => {
 
         it('test kí tự giảm dần', () => {
             cy.intercept('GET', Cypress.env('SaleCRM_URL')+'/contacts/*').as('getContacts');
@@ -16,13 +80,7 @@ describe('Kiểm tra chức năng sorting', () => {
             cy.wait('@getContacts');
             cy.get('th').contains('Lifecycle stage').click();
             cy.wait('@getContacts', {timeout:10000});
-            cy.get('span[id="sort_stage_id"]').should('have.attr', 'data-sort-type', 'desc').then(() => {
-                cy.get('#dataContactBind .table__contact--stage').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null)
-                    const sorted = [...values].sort((a:any, b:any) => a.localeCompare(b, 'vi', { sensitivity: 'base' }));
-                    expect(values).to.deep.eq(sorted);
-                })
-            });
+            verifySorting('alphabet','desc','span[id="sort_stage_id"]','.table__contact--stage');
         });
 
         it('test kí tự tăng dần', () => {
@@ -33,26 +91,14 @@ describe('Kiểm tra chức năng sorting', () => {
             cy.wait('@getContacts', {timeout:10000});
             cy.get('th').contains('Lifecycle stage').click();
             cy.wait('@getContacts', {timeout:10000});
-            cy.get('span[id="sort_stage_id"]').should('have.attr', 'data-sort-type', 'asc').then(() => {
-                cy.get('#dataContactBind .table__contact--stage').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null)
-                    const sorted = [...values].sort((a:any, b:any) => a.localeCompare(b, 'vi', { sensitivity: 'base' }));
-                    expect(values).to.deep.eq(sorted);
-                })
-            });
+            verifySorting('alphabet','asc','span[id="sort_stage_id"]','.table__contact--stage');
         });
 
         it('test ngày giảm dần', () => {
             cy.intercept('GET', Cypress.env('SaleCRM_URL')+'/contacts/*').as('getContacts');
             cy.visit(Cypress.env('SaleCRM_URL')+'contacts');
             cy.wait('@getContacts',{timeout:10000});
-            cy.get('span[id="sort_created_at"]').should('have.attr', 'data-sort-type', 'desc').then(() => {
-                cy.get('#dataContactBind .table__contact--created-date').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null);
-                    const sort = [...values].sort((a, b) => dayjs(b, 'DD/MM/YYYY').valueOf() - dayjs(a, 'DD/MM/YYYY').valueOf());
-                    expect(values).to.deep.equal(sort);
-                })
-            });
+            verifySorting('date','desc','span[id="sort_created_at"]','.table__contact--created-date');
         });
 
         it('test ngày tăng dần', () => {
@@ -61,43 +107,7 @@ describe('Kiểm tra chức năng sorting', () => {
             cy.wait('@getContacts',{timeout:10000});
             cy.get('th').contains('Created date').click();
             cy.wait('@getContacts', {timeout:10000});
-            cy.get('span[id="sort_created_at"]').should('have.attr', 'data-sort-type', 'asc').then(() => {
-                cy.get('#dataContactBind .table__contact--created-date').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null);
-                    const sort = [...values].sort((a, b) => dayjs(a, 'DD/MM/YYYY').valueOf() - dayjs(b, 'DD/MM/YYYY').valueOf());
-                    expect(values).to.deep.equal(sort);
-                })
-            });
-        });
-    })
-
-    //Chỉ hoạt động với acc Admin role.
-     describe('Sorting Kpis-point page', () => {
-        it('test số giảm dần', () => {
-            cy.visit(Cypress.env('SaleCRM_URL')+'kpis');
-            cy.get('th').contains('Monthly KPI goal').click();
-            cy.get('span[id="monthly_kpi_goal"]').should('have.attr', 'data-sort-type', 'desc').then(() => {
-                cy.get('[class^="goal-kpi-"]').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null)
-                    const numericValues = values.map(v => v ? Number(v) : null)
-                    const sorted = [...numericValues].sort((a, b) => (b ?? -Infinity) - (a ?? -Infinity))
-                    expect(numericValues).to.deep.eq(sorted)
-                })
-            });
-        });
-
-        it('test số tăng dần', () => {
-            cy.visit(Cypress.env('SaleCRM_URL')+'kpis');
-            cy.get('th').contains('Monthly KPI goal').click();
-            cy.get('th').contains('Monthly KPI goal').click();
-            cy.get('span[id="monthly_kpi_goal"]').should('have.attr', 'data-sort-type', 'asc').then(() => {
-                cy.get('[class^="goal-kpi-"]').then($cells => {
-                    const values = [...$cells].map(c => c.innerText.trim() || null);
-                    const numericValues = values.map(v => v ? Number(v) : null);
-                    const sorted = [...numericValues].sort((a, b) => (a ?? -Infinity) - (b ?? -Infinity));
-                    expect(numericValues).to.deep.eq(sorted);
-                })
-            });
+            verifySorting('date','asc','span[id="sort_created_at"]','.table__contact--created-date');
         });
     })
 })
